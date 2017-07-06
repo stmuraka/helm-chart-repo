030629aec31c9ea79f365da549443da1842296bd
Shaun Murakami <stmuraka@us.ibm.com>
2017-07-05 17:08:00 -0700

    manual chart repo in cluster

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+# INGRESS_SUBDOMAIN - kubernetes cluster subdomain
+_sig() {
+    echo "Caught signal..."
+    kill -TERM `cat /var/run/nginx.pid` 2>/dev/null
+}
+
+trap _sig SIGHUP SIGINT SIGTERM
+
+echo "Starting Nginx..."
+#nginx -g "daemon on;"
+nginx
+
+# Generate helm index.yaml each time the chart directory changes
+#while inotifywait -r -qq -e modify,move,create,delete /usr/share/nginx/html/charts; do
+while inotifywait -r -q -e modify,move,create,delete ${CHART_DIR}; do
+    helm repo index ${CHART_DIR} --url http://${INGRESS_SUBDOMAIN}/charts/
+done
